2ca807a54823fc0d86f5b670bcae2f17173967bc
Bijo Mariyil <bijomariyil@gmail.com>
2016-10-02 23:02:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,28 +3,61 @@ var morgan = require('morgan');
 var path = require('path');
 var app = express();
 app.use(morgan('combined'));
-var content = {
+var article1 = {
     title: "Article One | Bijo Mariyil",
     heading: "ARTICLE O N E",
     date: "02nd october 2016",
     content: `
     <p>
-        Hi Everybody, I am Little busy with my duties and responsibilities and also late. Anyway this is my first Article.
+       Hi Everybody, I am Little busy with my duties and responsibilities and also late. Anyway this is my first Article.
     </p>
     <p>
         Hi Everybody, I am a bit late. Trying to do fast. Anyway this is my First Article.
     </p>
     <p>
         Hi Everybody, At last this is my First Article.
-    </p>
-    `
+    </p> `
 };
+function createtemplate (data) {
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    
+var htmltemplate=`
+<html>
+   <title>
+       ${title}
+   </title> 
+   <meta name= "viewport" content="width=device-width, initial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+    <body>
+            <div class="container">
+                <div>
+                    <a href="/"> Home </a>
+                </div>
+                <hr/>
+               </h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+    </body>
+</html>
+`;
+return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get ('/Article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Article1.html'));
+  res.send(createtemplate(Article1));
 });
 
 app.get ('/Article-two', function (req, res) {
@@ -34,11 +67,9 @@ app.get ('/Article-two', function (req, res) {
 app.get ('/Article-three', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'Article3.html'));
 });
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
