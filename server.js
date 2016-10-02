4a332bb4f5ed0c87402c3067542ebfaa4229b31a
Bijo Mariyil <bijomariyil@gmail.com>
2016-10-02 23:58:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/', function (req, res) {
 app.get ('/:articlename', function (req, res) {
     //articlename=articleone
     //articles(articlename)={}content object of article one
-    var articlename=req.params.articlename;
+    var articlename=rec.params.articlename;
   res.send(createtemplate(Articles(articlename)));
 });
 
