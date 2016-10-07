841234b300bb4a709f2279c7bd0f75eb94558c2c
Bijo Mariyil <bijomariyil@gmail.com>
2016-10-07 11:59:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML = "Changed TEXT";
 var img=document.getElementById("madi");
 var marginleft = 0;
 function moveRight() { 
-    marginleft = marginleft + 5;
+    marginleft = marginleft = 5;
     img.style.marginleft = marginleft = "px";
 }
 img.onclick = function() {
