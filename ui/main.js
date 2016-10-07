f0c83f9c8a8fccb51d58dba85dafd3f01cd212a5
Bijo Mariyil <bijomariyil@gmail.com>
2016-10-07 13:17:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,12 @@ console.log('Loaded!');
 var element=document.getElementById("main-text"
 );
 element.innerHTML = "Changed TEXT";
-
 var img = document.getElementById ("madi");
 var marginleft = 0;
 function moveRight() { 
     marginleft = marginleft + 5;
  img.style.marginleft = marginleft + "px";
 }
-img.onclick = function() {
+img.onclick = function( ) {
     var interval = setInterval(moveRight, 50);
 };
